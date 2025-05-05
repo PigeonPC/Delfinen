492fa1bf9054bcd858cd8f105d9241fae3f7ee9e
Guts <jonathanbirkstrom@gmail.com>
2025-05-05 14:11:47 +0200

    hi

--- src/dev/pigeonboys/swimclub/FileHandler.java
+++ src/dev/pigeonboys/swimclub/FileHandler.java
@@ -1,4 +1,28 @@
-package src.dev.pigeonboys.swimclub;
+package dev.pigeonboys.swimclub;
+
+import dev.pigeonboys.member.Member;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FileHandler {
+    private static final String FILE_PATH = "members.txt";
+
+    public void saveMember(Member member) {
+
+        try(FileWriter fw = new FileWriter(FILE_PATH, true));
+        BufferedWriter writer = new BufferedWriter(fw);
+        PrintWriter out = new PrintWriter(writer)) {
+
+            out.println(member.getId() + "|" + member.getName() + "|" +
+                    member.getAddress());
+        } catch(IOException e) {
+            System.err.println("Error adding member" + e.getMessage());
+        }
+
+    }
+
+
 }
